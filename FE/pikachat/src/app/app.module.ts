9afcb0615fbe8216eda38e1fcefe423537681cc2
Diamond Eevee <diamond.ms.eevee@gmail.com>
2023-02-18 12:03:17 +0100

    INIT

---
+++ FE/pikachat/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChatComponent } from './chat/chat.component';
+import { ServersComponent } from './servers/servers.component';
+import { ChannelsComponent } from './channels/channels.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    ServersComponent,
+    ChannelsComponent,
+    MessagesComponent
+  ],
+  imports: [
+    MatButtonModule,
+    MatSidenavModule,
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
